4a1b8cd952236a62b87dc792d29aacd8c9ec73ab
Joao Piologo <joaotpiologo@hotmail.com>
2025-04-23 22:45:33 -0300

    Update: Fonts to h1

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import { Heading } from "./components/Heading";
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10"> 
+            <Heading>
+              <span>
+                A spotless home whenever you need it,
+              </span>
+              <br />
+              <span className="inline-bloc bg-gradient-to-r from-sky-600 to-sky-950 text-transparent bg-clip-text">
+                Book now and leave the rest to us.
+              </span>
+            </Heading>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  )
+}
